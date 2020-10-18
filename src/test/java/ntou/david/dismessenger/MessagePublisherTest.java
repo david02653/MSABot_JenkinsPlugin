9af2f356ead93dbfbe3e5657516eee9aaa52a6ed
otonashi <david02653@gmail.com>
2020-10-19 01:08:50 +0800

    add hpi file in

---
+++ src/test/java/ntou/david/dismessenger/MessagePublisherTest.java
@@ -0,0 +1,10 @@
+package ntou.david.dismessenger;
+
+import org.junit.Rule;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class MessagePublisherTest {
+
+    @Rule
+    public JenkinsRule jenkins = new JenkinsRule();
+}
